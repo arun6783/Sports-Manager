a98d3927f5e6360cd752d055d49a24d16fe863b4
CompareNInsure <arunkmr1983@gmail.com>
2020-03-17 06:59:06 +0000

    added redux

--- src/App.js
+++ src/App.js
@@ -3,43 +3,40 @@ import './App.css';
 import HomePage from "./Pages/HomePage";
 import AboutUs from './Pages/About-Us'
 import { Switch, Route } from 'react-router-dom';
+
+import{connect} from 'react-redux';
+
 import Header from "./Components/Header-Component/Header-Component";
 import SignInAndSignUp from "./Pages/Sign-In-sign-up";
 import { auth , createUserProfileDocument } from './firebase/firebase.utils';
+
+import { setCurrentUser} from './redux/user/user.actions'
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      currentUser:null
-    }
-  }
+
 
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=> {  
         // createUserProfileDocument(user);
 
         if(userAuth){
           const userRef = createUserProfileDocument(userAuth);
           (await userRef).onSnapshot(snapShot=>{
-            this.setState({
-              currentUser: { 
+            setCurrentUser({ 
                 id: snapShot.id,
                 ...snapShot.data()
-              }
-            }, ()=>{
-
-              console.log(this.state);
-            });
+              });
           });
     
         }
         //console.log('this line incase if user auth is null', userAuth);
         //but this line of code will be called as soon as user logs in , because creaete userprofiledocument onsnapshot is async.
         //here user auth will be just the object what firebase returns.
-        this.setState({currentUser: userAuth});
+        setCurrentUser(userAuth);
     });
 
   }
@@ -50,7 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header currentUser={this.state.currentUser}></Header>
+        <Header ></Header>
 
         <Switch>
           <Route exact path='/' component={HomePage} />
@@ -62,4 +59,8 @@ class App extends React.Component {
   }
 
 }
-export default App;
+
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(null,mapDispatchToProps)(App);
